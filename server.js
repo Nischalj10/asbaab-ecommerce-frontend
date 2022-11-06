50a940875b1ad787022af2d9de2c25d268949d77
Nischal Jain <55933460+Nischalj10@users.noreply.github.com>
2022-11-06 16:37:19 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require("express");
 var app = express();
 app.use(express.static(__dirname + "/"));
-app.listen(process.env.PORT || 8080);
+app.listen(process.env.PORT || 8080, '0.0.0.0');
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("build"));
